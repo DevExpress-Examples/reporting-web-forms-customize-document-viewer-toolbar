65cfbc9a5fa867e66ebd151e751dd5648925a57d
vasyaste <vasiliy.stepanov@devexpress.com>
2023-12-15 13:09:08 +0400

    Upgrade tp v20.2

---
+++ CS/DXWebApplication1/CustomReportProvider.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.XtraReports.Services;
+using DevExpress.XtraReports.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.ServiceModel;
+using System.Web;
+
+namespace DXWebApplication1 {
+    public class CustomReportProvider : DevExpress.XtraReports.Services.IReportProvider {
+        protected string ReportDirectory { get; set; }
+        const string REPORT_FILE_EXTENSION = ".repx";
+
+        public CustomReportProvider(string reportDirectory) {
+            this.ReportDirectory = reportDirectory;
+        }
+
+        public XtraReport GetReport(string id, ReportProviderContext context) {
+            try {
+                var reportBytes = File.ReadAllBytes(Path.Combine(ReportDirectory, id + REPORT_FILE_EXTENSION));
+                using (var memoryStream = new MemoryStream(reportBytes)) {
+                    return XtraReport.FromStream(memoryStream);
+                }
+            }
+            catch (Exception) {
+                throw new FaultException(new FaultReason(string.Format("Could not find report '{0}'.", id)), new FaultCode("Server"), "GetReport");
+            }
+        }
+    }
+}
--- CS/DXWebApplication1/Global.asax.cs
+++ CS/DXWebApplication1/Global.asax.cs
@@ -1,17 +1,17 @@
 using System;
 using System.IO;
 using System.Web;
+using DevExpress.XtraReports.Services;
 using DevExpress.XtraReports.Web;
 
 namespace DXWebApplication1 {
     public class Global_asax : System.Web.HttpApplication {
         void Application_Start(object sender, EventArgs e) {
-            System.Web.Routing.RouteTable.Routes.MapPageRoute("defaultRoute", "", "~/Viewer.aspx");
             DevExpress.XtraReports.Configuration.Settings.Default.UserDesignerOptions.DataBindingMode = DevExpress.XtraReports.UI.DataBindingMode.Expressions;
             DevExpress.XtraReports.Web.WebDocumentViewer.Native.WebDocumentViewerBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default;
             DevExpress.XtraReports.Web.QueryBuilder.Native.QueryBuilderBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default;
-            DevExpress.XtraReports.Web.ReportDesigner.Native.ReportDesignerBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default;
-            DevExpress.XtraReports.Web.Extensions.ReportStorageWebExtension.RegisterExtensionGlobal(new ReportStorageWebExtension1(Path.Combine(HttpRuntime.AppDomainAppPath, "Reports")));
+            DevExpress.XtraReports.Web.ReportDesigner.Native.ReportDesignerBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default;            
+            DevExpress.XtraReports.Web.WebDocumentViewer.DefaultWebDocumentViewerContainer.RegisterSingleton<IReportProvider>(new CustomReportProvider(Server.MapPath("~/Reports")));
             ASPxReportDesigner.StaticInitialize();
         }
     }
